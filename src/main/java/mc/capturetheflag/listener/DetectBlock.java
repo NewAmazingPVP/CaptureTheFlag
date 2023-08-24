0b876be4811e29b65cb9ae9b05b97d159ee9eac3
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-24 14:26:47 -0400

    1

--- src/main/java/mc/capturetheflag/listener/DetectBlock.java
+++ src/main/java/mc/capturetheflag/listener/DetectBlock.java
@@ -7,17 +7,19 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class DetectBlock implements Listener {
-
+    
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
-        Location location = player.getLocation();
-        Block block = location.getBlock();
+        Location to = event.getTo();
 
-        if (block.getType() == Material.SPONGE) {
+        if (to.getBlock().getType() == Material.SPONGE) {
             player.sendMessage("test");
-        }
+            player.playSound(player.getLocation(), "entity.blaze.shoot", 1.0f, 1.0f);
+            player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 20, 0));
         }
     }
