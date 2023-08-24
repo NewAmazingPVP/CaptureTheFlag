6f8b742c4b28bbf1e0e361907e167c0c547efa0e
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-08-24 13:52:38 -0400

    push1

---
+++ src/main/java/mc/capturetheflag/listener/DetectBlock.java
@@ -0,0 +1,12 @@
+package mc.capturetheflag.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class DetectBlock implements Listener {
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+    }
+}
