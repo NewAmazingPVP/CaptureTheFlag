7bb3c7fca0bf21a7c9532e88037bf3a7f98646d0
NewAmazingPVP <gamingofpat@gmail.com>
2023-08-24 14:03:26 -0400

    Update CaptureTheFlag.java

--- src/main/java/mc/capturetheflag/CaptureTheFlag.java
+++ src/main/java/mc/capturetheflag/CaptureTheFlag.java
@@ -1,12 +1,13 @@
 package mc.capturetheflag;
 
+import mc.capturetheflag.listener.DetectBlock;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CaptureTheFlag extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(new DetectBlock(), this);
 
     }
 }
