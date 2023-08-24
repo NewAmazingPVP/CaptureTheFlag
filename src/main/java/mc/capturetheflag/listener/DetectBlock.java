b029ef995ceaa9a542fdfeb3d523ab82fb0881ad
NewAmazingPVP <gamingofpat@gmail.com>
2023-08-24 15:11:23 -0400

    Update DetectBlock.java

--- src/main/java/mc/capturetheflag/listener/DetectBlock.java
+++ src/main/java/mc/capturetheflag/listener/DetectBlock.java
@@ -6,12 +6,13 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class DetectBlock implements Listener {
-    
+
 
 }
