429bb7e7b1c119e69aeb9ea5b6739da6306f6714
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-24 14:02:15 -0400

    added something idk...

--- src/main/java/mc/capturetheflag/listener/DetectBlock.java
+++ src/main/java/mc/capturetheflag/listener/DetectBlock.java
@@ -1,5 +1,9 @@
 package mc.capturetheflag.listener;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -8,5 +12,12 @@ public class DetectBlock implements Listener {
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        Location location = player.getLocation();
+        Block block = location.getBlock();
+
+        if (block.getType() == Material.SPONGE) {
+            player.sendMessage("test");
+        }
+        }
     }
-}
