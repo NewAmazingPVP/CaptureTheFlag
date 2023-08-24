dd0ede3f8b048c530f904ec298ca2421dc88625e
NewAmazingPVP <gamingofpat@gmail.com>
2023-08-24 16:23:44 -0400

    Update Craft_table_detect.java

--- src/main/java/mc/capturetheflag/listener/Craft_table_detect.java
+++ src/main/java/mc/capturetheflag/listener/Craft_table_detect.java
@@ -6,6 +6,7 @@ import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.potion.PotionEffect;
@@ -13,11 +14,11 @@ import org.bukkit.potion.PotionEffectType;
 
 public class Craft_table_detect implements Listener {
 
-    @EventHandler
-    public void onPlayerMove(PlayerMoveEvent event) {
-        Player player = event.getPlayer();
 
-        if (player.openWorkbench(player.closeInventory();
+    @EventHandler
+    public void onInventoryOpen(InventoryOpenEvent event) {
+        if (event.getInventory().getType() == org.bukkit.event.inventory.InventoryType.WORKBENCH) {
+            event.setCancelled(true); // Cancel the event to prevent opening the crafting table
         }
     }
 }
