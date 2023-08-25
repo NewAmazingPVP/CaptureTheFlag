664c79f6b8bff5742bb3c95d3c1ae7aa7f285832
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-25 16:20:22 -0400

    test 2

--- src/main/java/mc/capturetheflag/listener/CancelArmorTakeoff.java
+++ src/main/java/mc/capturetheflag/listener/CancelArmorTakeoff.java
@@ -12,26 +12,28 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class CancelArmorTakeoff implements Listener {
 
+        @EventHandler
+        public void onInventoryClick(InventoryClickEvent event) {
+            if (event.getWhoClicked() instanceof Player) {
+                Player player = (Player) event.getWhoClicked();
+                ItemStack clickedItem = event.getCurrentItem();
 
-    @EventHandler
-    public void onInventoryClick(InventoryClickEvent event) {
-        if (event.getWhoClicked() instanceof Player) {
-            Player player = (Player) event.getWhoClicked();
-            ItemStack clickedItem = event.getCurrentItem();
-
-            if (clickedItem != null && clickedItem.getType().name().contains("HELMET")) {
-                event.setCancelled(true);
-                player.sendMessage("You cannot remove your helmet!");
-            } else if (clickedItem != null && clickedItem.getType().name().contains("CHESTPLATE")) {
-                event.setCancelled(true);
-                player.sendMessage("You cannot remove your chestplate!");
-            } else if (clickedItem != null && clickedItem.getType().name().contains("LEGGINGS")) {
-                event.setCancelled(true);
-                player.sendMessage("You cannot remove your leggings!");
-            } else if (clickedItem != null && clickedItem.getType().name().contains("BOOTS")) {
-                event.setCancelled(true);
-                player.sendMessage("You cannot remove your boots!");
+                if (clickedItem != null && clickedItem.getType().name().contains("HELMET")) {
+                    event.setCancelled(true);
+                    player.sendMessage("You cannot remove your helmet!");
+                } else if (clickedItem != null && clickedItem.getType().name().contains("CHESTPLATE")) {
+                    event.setCancelled(true);
+                    player.sendMessage("You cannot remove your chestplate!");
+                } else if (clickedItem != null && clickedItem.getType().name().contains("LEGGINGS")) {
+                    event.setCancelled(true);
+                    player.sendMessage("You cannot remove your leggings!");
+                } else if (clickedItem != null && clickedItem.getType().name().contains("BOOTS")) {
+                    event.setCancelled(true);
+                    player.sendMessage("You cannot remove your boots!");
+                } else if (clickedItem != null && clickedItem.getType().name().contains("ARMOR")) {
+                    event.setCancelled(true);
+                    player.sendMessage("You cannot interact with armor items!");
+                }
             }
         }
     }
-}
