a3996a6ceb30db6247840a8e3975bdf508f9e639
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-25 14:03:25 -0400

    1

---
+++ src/main/java/mc/capturetheflag/listener/RedTeamArmor.java
@@ -0,0 +1,50 @@
+package mc.capturetheflag.listener;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+public class RedTeamArmor implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        handlePlayer(player);
+    }
+
+    private void handlePlayer(Player player) {
+        Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+        Team team = scoreboard.getEntryTeam(player.getName());
+
+        if (team != null && team.getName().equals("red")) {
+            ItemStack boots = new ItemStack(Material.LEATHER_BOOTS);
+            LeatherArmorMeta bootsMeta = (LeatherArmorMeta) boots.getItemMeta();
+            bootsMeta.setColor(Color.RED);
+            boots.setItemMeta(bootsMeta);
+
+            ItemStack leggings = new ItemStack(Material.LEATHER_LEGGINGS);
+            LeatherArmorMeta leggingsMeta = (LeatherArmorMeta) leggings.getItemMeta();
+            leggingsMeta.setColor(Color.RED);
+            leggings.setItemMeta(leggingsMeta);
+
+            ItemStack chestplate = new ItemStack(Material.LEATHER_CHESTPLATE);
+            LeatherArmorMeta chestplateMeta = (LeatherArmorMeta) chestplate.getItemMeta();
+            chestplateMeta.setColor(Color.RED);
+            chestplate.setItemMeta(chestplateMeta);
+
+            player.getInventory().setBoots(boots);
+            player.getInventory().setLeggings(leggings);
+            player.getInventory().setChestplate(chestplate);
+        }
+    }
+
+}
