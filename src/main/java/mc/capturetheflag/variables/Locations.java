d7e11f3b13197d870df2da250e28e82d04a038c2
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-08-25 12:23:17 -0400

    test locations

---
+++ src/main/java/mc/capturetheflag/variables/Locations.java
@@ -0,0 +1,8 @@
+package mc.capturetheflag.variables;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+public class Locations {
+    public static Location lobbyLoc = new Location(Bukkit.getWorld("world"), 0, 195, 0);
+}
