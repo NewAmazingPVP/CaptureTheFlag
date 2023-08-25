f2cbe3c2b6982c4329c472705c4b5e9e48cf4cb6
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-25 11:55:52 -0400

    ;]

--- src/main/java/mc/capturetheflag/listener/ShearSheepTest.java
+++ src/main/java/mc/capturetheflag/listener/ShearSheepTest.java
@@ -15,8 +15,8 @@ public class ShearSheepTest implements Listener {
         if (event.getEntity() instanceof Sheep) {
             event.setCancelled(true);
             Sheep sheep = (Sheep) event.getEntity();
-            player.openWorkbench(player.getLocation(), true);
-            player.sendMessage(ChatColor.GOLD + "This is a crafting sheep ;]");
+            sheep.setHealth(0.0);
+            player.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Shit u killed it! That is NOT how you shear a sheep!");
         }
     }
 }
