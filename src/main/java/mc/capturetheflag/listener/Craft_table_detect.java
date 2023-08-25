719b7af21afad170a9c58e78aa71f6e90b9bec72
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-24 22:23:47 -0400

    1

--- src/main/java/mc/capturetheflag/listener/Craft_table_detect.java
+++ src/main/java/mc/capturetheflag/listener/Craft_table_detect.java
@@ -20,7 +20,7 @@ public class Craft_table_detect implements Listener {
     @EventHandler
     public void onInventoryOpen(InventoryOpenEvent event) {
         Player player = (Player) event.getPlayer();
-        if (!(event.getInventory().getType() == InventoryType.PLAYER)) if (!(event.getInventory().getType() == InventoryType.FURNACE)) {
+        if (!(event.getInventory().getType() == InventoryType.PLAYER)) if (!(event.getInventory().getType() == InventoryType.FURNACE)) if (!(event.getInventory().getType() == InventoryType.WORKBENCH)) {
             event.setCancelled(true); // Cancel the event to prevent opening the crafting table
             player.sendMessage(ChatColor.RED + "You can't open that!");
         }
