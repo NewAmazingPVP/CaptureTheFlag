56d37502d4c283cc21ee6abd1ba82fdd460c6e88
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-08-25 12:36:59 -0400

    Update KeepArmorOnNoTeam.java

--- src/main/java/mc/capturetheflag/listener/KeepArmorOnNoTeam.java
+++ src/main/java/mc/capturetheflag/listener/KeepArmorOnNoTeam.java
@@ -1,17 +1,12 @@
 package mc.capturetheflag.listener;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.InventoryType;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.LeatherArmorMeta;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
