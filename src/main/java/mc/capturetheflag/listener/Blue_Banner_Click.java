1e135295e2033524c400f855b2ba87cc442a571a
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-25 14:30:39 -0400

    1

--- src/main/java/mc/capturetheflag/listener/Blue_Banner_Click.java
+++ src/main/java/mc/capturetheflag/listener/Blue_Banner_Click.java
@@ -1,27 +1,32 @@
 package mc.capturetheflag.listener;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Particle;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class Blue_Banner_Click implements Listener {
-
-
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = (Player) event.getPlayer();
+        World world = player.getWorld();
         Location to = event.getTo();
         Location below = to.clone().subtract(0, 0, 0);
 
         if (below.getBlock().getType() == Material.BLUE_BANNER) {
             player.sendMessage(ChatColor.DARK_GREEN + "Blue Picked Up!");
             player.spawnParticle(Particle.COMPOSTER, player.getLocation().add(0, 1, 0), 100, 0.5, 0.5, 0.5, 0.1);
+            player.playSound(player.getLocation(), "minecraft:block.note_block.bit", 1.0f, 2.0f);
+            int x = -81;
+            int y = 71;
+            int z = 0;
+            Material blockType = Material.AIR;
+
         }
     }
 }
