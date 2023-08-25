4057f6f1314ad1d25de5f357f3b8bb2dedf7c5d7
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-25 12:14:57 -0400

    1

--- src/main/java/mc/capturetheflag/listener/ShearSheepTest.java
+++ src/main/java/mc/capturetheflag/listener/ShearSheepTest.java
@@ -1,6 +1,8 @@
 package mc.capturetheflag.listener;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Sheep;
 import org.bukkit.event.EventHandler;
@@ -15,8 +17,10 @@ public class ShearSheepTest implements Listener {
         if (event.getEntity() instanceof Sheep) {
             event.setCancelled(true);
             Sheep sheep = (Sheep) event.getEntity();
+            sheep.getWorld().spawnParticle(Particle.REDSTONE, sheep.getLocation(), 10, 0.6, 0.6, 0.6, new Particle.DustOptions(Color.RED, 3));
             sheep.setHealth(0.0);
             player.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Shit u killed it! That is NOT how you shear a sheep!");
+            player.playSound(player.getLocation(), "minecraft:entity.slime.death", 1.0f, 1.0f);
         }
     }
 }
