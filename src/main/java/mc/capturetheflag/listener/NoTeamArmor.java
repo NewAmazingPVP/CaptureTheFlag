85359ceafb600a583e70610f8eeefa40519e3288
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-25 12:31:34 -0400

    1

---
+++ src/main/java/mc/capturetheflag/listener/NoTeamArmor.java
@@ -0,0 +1,37 @@
+package mc.capturetheflag.listener;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+public class NoTeamArmor implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        handlePlayer(player);
+    }
+
+    private void handlePlayer(Player player) {
+        Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+        Team team = scoreboard.getEntryTeam(player.getName());
+
+        if (team != null && team.getName().equals("Not In A Team")) {
+            ItemStack boots = new ItemStack(Material.LEATHER_BOOTS);
+            LeatherArmorMeta meta = (LeatherArmorMeta) boots.getItemMeta();
+            meta.setColor(Color.GRAY);
+            boots.setItemMeta(meta);
+
+            player.getInventory().setBoots(boots);
+        }
+    }
+}
