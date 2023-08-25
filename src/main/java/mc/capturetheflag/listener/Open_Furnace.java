f840c03284a3525f4c0e02ee8a4641c787f82ee4
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-24 22:08:47 -0400

    1

---
+++ src/main/java/mc/capturetheflag/listener/Open_Furnace.java
@@ -0,0 +1,28 @@
+package mc.capturetheflag.listener;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class Open_Furnace implements Listener {
+
+
+    @EventHandler
+    public void onInventoryOpen(InventoryOpenEvent event) {
+        Player player = (Player) event.getPlayer();
+        if ((event.getInventory().getType() == InventoryType.FURNACE)) {
+            event.setCancelled(true); // Cancel the event to prevent opening the crafting table
+            player.openWorkbench(player.getLocation(), true);
+        }
+    }
+}
