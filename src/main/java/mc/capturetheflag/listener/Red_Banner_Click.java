7cece4c2f6ede8c0985dc791f46f86847ff3def1
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-08-25 21:52:15 -0400

    fixes v3

--- src/main/java/mc/capturetheflag/listener/Red_Banner_Click.java
+++ src/main/java/mc/capturetheflag/listener/Red_Banner_Click.java
@@ -16,29 +16,29 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scoreboard.Team;
 
 import java.util.HashMap;
 import java.util.UUID;
+
+import static mc.capturetheflag.game.GetTeam.getCurrentTeam;
 
 public class Red_Banner_Click implements Listener {
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = (Player) event.getPlayer();
+        Team team = getCurrentTeam(player);
         World world = player.getWorld();
         Location to = event.getTo();
         Location below = to.clone().subtract(0, 0, 0);
 
-        if (below.getBlock().getType() == Material.RED_BANNER) {
-            player.sendMessage(ChatColor.DARK_RED + "Red Picked Up!");
+        if (below.getBlock().getType() == Material.RED_BANNER && team != null && team.getName().equalsIgnoreCase("blue")) {
+            player.sendMessage(ChatColor.DARK_RED + "Red Flag Picked Up!");
             player.spawnParticle(Particle.COMPOSTER, player.getLocation().add(0, 1, 0), 100, 0.5, 0.5, 0.5, 0.1);
             player.playSound(player.getLocation(), "minecraft:block.note_block.bit", 1.0f, 2.0f);
-            int x = 81;
-            int y = 71;
-            int z = 0;
-            Material blockType = Material.AIR;
-            Location location = new Location(world, 81, 71, 0);
-            location.getBlock().setType(blockType);
-            ItemStack blueBanner = new ItemStack(Material.RED_BANNER);
+            Location location = player.getLocation();
+            location.getBlock().setType(Material.AIR);
+            ItemStack blueBanner = new ItemStack(Material.BLUE_BANNER);
             player.getEquipment().setItem(EquipmentSlot.HEAD, blueBanner);
             player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 999999999, 1, true));
             Sound sound = Sound.ENTITY_ENDER_DRAGON_GROWL;
