ba11fc6ee81c42758effe57bd1a3d9a2138161bc
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-25 21:55:17 -0400

    1

--- src/main/java/mc/capturetheflag/command/jail_ctf.java
+++ src/main/java/mc/capturetheflag/command/jail_ctf.java
@@ -1,12 +1,12 @@
 package mc.capturetheflag.command;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.Location;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 
 import static mc.capturetheflag.variables.Locations.jail;
 
@@ -18,6 +18,25 @@ public class jail_ctf implements CommandExecutor {
             assert player != null;
             player.teleport(jail);
             player.sendMessage(ChatColor.RED + "You were killed!");
+
+            ItemStack boots = new ItemStack(Material.LEATHER_BOOTS);
+            LeatherArmorMeta bootsMeta = (LeatherArmorMeta) boots.getItemMeta();
+            bootsMeta.setColor(Color.ORANGE);
+            boots.setItemMeta(bootsMeta);
+
+            ItemStack leggings = new ItemStack(Material.LEATHER_LEGGINGS);
+            LeatherArmorMeta leggingsMeta = (LeatherArmorMeta) leggings.getItemMeta();
+            leggingsMeta.setColor(Color.ORANGE);
+            leggings.setItemMeta(leggingsMeta);
+
+            ItemStack chestplate = new ItemStack(Material.LEATHER_CHESTPLATE);
+            LeatherArmorMeta chestplateMeta = (LeatherArmorMeta) chestplate.getItemMeta();
+            chestplateMeta.setColor(Color.ORANGE);
+            chestplate.setItemMeta(chestplateMeta);
+
+            player.getInventory().setBoots(boots);
+            player.getInventory().setLeggings(leggings);
+            player.getInventory().setChestplate(chestplate);
         } else {
             sender.sendMessage("Specify player");
         }
