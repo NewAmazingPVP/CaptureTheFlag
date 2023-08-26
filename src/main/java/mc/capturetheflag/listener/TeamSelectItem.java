328493c8ba34985efdd9b6c045084089ca4894d2
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-26 11:06:18 -0400

    1

--- src/main/java/mc/capturetheflag/listener/TeamSelectItem.java
+++ src/main/java/mc/capturetheflag/listener/TeamSelectItem.java
@@ -71,7 +71,7 @@ public class TeamSelectItem implements Listener {
                 teamselect.setItem(9,g9);
 
                 ItemStack g10 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
-                teamselect.setItem(10,g10);
+                teamselect.setItem(10,g10);*/
 
                 ItemStack g12 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
                 teamselect.setItem(12,g12);
@@ -116,7 +116,7 @@ public class TeamSelectItem implements Listener {
                 teamselect.setItem(26,g26);
 
                 ItemStack g27 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
-                teamselect.setItem(27,g27);*/
+                teamselect.setItem(27,g27);
 
 
 
