d51e77a9ffc6ccc98e0bd20266b6444bd4d60103
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-26 10:48:18 -0400

    lol

--- src/main/java/mc/capturetheflag/listener/TeamSelectItem.java
+++ src/main/java/mc/capturetheflag/listener/TeamSelectItem.java
@@ -26,24 +26,104 @@ public class TeamSelectItem implements Listener {
                 // Perform your desired action here
                 Player p = event.getPlayer();
                 player.playSound(player.getLocation(), "minecraft:block.note_block.bit", 1.0f, 2.0f);
-                Inventory inventory = Bukkit.createInventory(p,27, ChatColor.DARK_AQUA + "[Select Team]");
-
+                Inventory teamselect = Bukkit.createInventory(p,27, ChatColor.DARK_AQUA + "[Select Team]");
 
                 ItemStack rteam = new ItemStack(Material.RED_CONCRETE);
                 ItemMeta RMeta = rteam.getItemMeta();
                 RMeta.setDisplayName(ChatColor.YELLOW + "" + ChatColor.BOLD + "Click" + ChatColor.DARK_RED + " To Join Red");
                 rteam.setItemMeta(RMeta);
-                inventory.setItem(12, rteam);
-
+                teamselect.setItem(11, rteam);
 
                 ItemStack bteam = new ItemStack(Material.BLUE_CONCRETE);
                 ItemMeta BMeta = bteam.getItemMeta();
                 BMeta.setDisplayName(ChatColor.YELLOW + "" + ChatColor.BOLD + "Click" + ChatColor.DARK_BLUE + " To Join Blue");
                 bteam.setItemMeta(BMeta);
-                inventory.setItem(16,bteam);
+                teamselect.setItem(15,bteam);
+
+                ItemStack g0 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g0);
+
+                ItemStack g1 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g1);
+
+                ItemStack g2 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g2);
+
+                ItemStack g3 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g3);
+
+                ItemStack g4 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g4);
+
+                ItemStack g5 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g5);
+
+                ItemStack g6 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g6);
+
+                ItemStack g7 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g7);
+
+                ItemStack g8 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g8);
+
+                ItemStack g9 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g9);
+
+                ItemStack g10 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g10);
+
+                ItemStack g11 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g11);
+
+                ItemStack g12 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g12);
+
+                ItemStack g13 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g13);
+
+                ItemStack g14 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g14);
+
+                ItemStack g16 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g16);
+
+                ItemStack g17 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g17);
+
+                ItemStack g18 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g18);
+
+                ItemStack g19 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g19);
+
+                ItemStack g20 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g20);
+
+                ItemStack g21 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g21);
+
+                ItemStack g22 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g22);
+
+                ItemStack g23 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g23);
+
+                ItemStack g24 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g24);
+
+                ItemStack g25 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g25);
+
+                ItemStack g26 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g26);
+
+                ItemStack g27 = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+                teamselect.setItem(0,g27);
 
 
-                p.openInventory(inventory);
+
+                p.openInventory(teamselect);
 
             }
         }
