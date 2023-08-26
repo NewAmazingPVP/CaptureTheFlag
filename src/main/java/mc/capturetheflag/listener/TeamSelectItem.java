77351a39c9088d082ea1a8751b99e12f39ae2b1d
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-26 10:02:59 -0400

    1

--- src/main/java/mc/capturetheflag/listener/TeamSelectItem.java
+++ src/main/java/mc/capturetheflag/listener/TeamSelectItem.java
@@ -1,11 +1,15 @@
 package mc.capturetheflag.listener;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
 
@@ -17,9 +21,12 @@ public class TeamSelectItem implements Listener {
             Player player = event.getPlayer();
             ItemStack heldItem = player.getInventory().getItemInMainHand();
 
-            if (heldItem.getType() == Material.DIAMOND_SWORD) {
+            if (heldItem.getType() == Material.NOTE_BLOCK) {
                 // Perform your desired action here
-                player.sendMessage("You right-clicked while holding a diamond sword!");
+                Player p = event.getPlayer();
+                player.playSound(player.getLocation(), "minecraft:block.note_block.bit", 1.0f, 2.0f);
+                Inventory inventory = Bukkit.createInventory(p,27, ChatColor.DARK_AQUA + "[Select Team]");
+
             }
         }
     }
