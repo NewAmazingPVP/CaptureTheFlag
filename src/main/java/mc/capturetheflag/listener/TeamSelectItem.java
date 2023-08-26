97fff3bbba0e94652e168a780bf58e2c44259936
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-26 19:45:27 -0400

    1

--- src/main/java/mc/capturetheflag/listener/TeamSelectItem.java
+++ src/main/java/mc/capturetheflag/listener/TeamSelectItem.java
@@ -22,7 +22,7 @@ public class TeamSelectItem implements Listener {
             Player player = event.getPlayer();
             ItemStack heldItem = player.getInventory().getItemInMainHand();
 
-            if (heldItem.getType() == Material.NOTE_BLOCK) {
+            if (heldItem.getType() == Material.COMPASS) {
                 // Perform your desired action here
                 Player p = event.getPlayer();
                 player.playSound(player.getLocation(), "minecraft:block.note_block.bit", 1.0f, 2.0f);
