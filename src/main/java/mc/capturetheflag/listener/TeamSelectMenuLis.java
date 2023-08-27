ca9e2cc739171e60ca1bc6ad928b51b9f7eedf8f
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-27 10:45:22 -0400

    1

--- src/main/java/mc/capturetheflag/listener/TeamSelectMenuLis.java
+++ src/main/java/mc/capturetheflag/listener/TeamSelectMenuLis.java
@@ -48,21 +48,21 @@ public class TeamSelectMenuLis implements Listener {
 
                 ItemStack boots = new ItemStack(Material.LEATHER_BOOTS);
                 LeatherArmorMeta bootsMeta = (LeatherArmorMeta) boots.getItemMeta();
-                bootsMeta.setColor(Color.BLACK);
-                bootsMeta.setDisplayName(ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "Black Armor");
+                bootsMeta.setColor(Color.WHITE);
+                bootsMeta.setDisplayName(ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "White Armor");
                 boots.setItemMeta(bootsMeta);
 
                 ItemStack leggings = new ItemStack(Material.LEATHER_LEGGINGS);
                 LeatherArmorMeta leggingsMeta = (LeatherArmorMeta) leggings.getItemMeta();
-                leggingsMeta.setDisplayName(ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "Black Armor");
-                leggingsMeta.setColor(Color.BLACK);
+                leggingsMeta.setDisplayName(ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "White Armor");
+                leggingsMeta.setColor(Color.WHITE);
                 leggings.setItemMeta(leggingsMeta);
 
 
                 ItemStack chestplate = new ItemStack(Material.LEATHER_CHESTPLATE);
                 LeatherArmorMeta chestplateMeta = (LeatherArmorMeta) chestplate.getItemMeta();
-                chestplateMeta.setColor(Color.BLACK);
-                chestplateMeta.setDisplayName(ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "Black Armor");
+                chestplateMeta.setColor(Color.WHITE);
+                chestplateMeta.setDisplayName(ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "White Armor");
                 chestplate.setItemMeta(chestplateMeta);
 
                 player.getInventory().setBoots(boots);
