22ffae7e36ce37a8e6afe11b60f2c8907efd4593
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-26 20:22:11 -0400

    1

--- src/main/java/mc/capturetheflag/listener/TeamSelectMenuLis.java
+++ src/main/java/mc/capturetheflag/listener/TeamSelectMenuLis.java
@@ -39,8 +39,8 @@ public class TeamSelectMenuLis implements Listener {
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "/leave");
                 Player player = (Player) event.getView().getPlayer();
                 player.performCommand("leave");
-                player.playSound(player.getLocation(), "minecraft:block.note_block.pling", 1.0f, 5.0f);
-                player.getWorld().spawnParticle(Particle.REDSTONE, player.getLocation(), 10, 0.6, 0.6, 0.0, new Particle.DustOptions(Color.BLACK, 3));
+                player.playSound(player.getLocation(), "minecraft:block.note_block.pling", 1.0f, 0.0f);
+                player.getWorld().spawnParticle(Particle.REDSTONE, player.getLocation(), 10, 0.6, 0.6, 0.6, new Particle.DustOptions(Color.BLACK, 3));
                 ItemStack boots = new ItemStack(Material.LEATHER_BOOTS);
                 LeatherArmorMeta bootsMeta = (LeatherArmorMeta) boots.getItemMeta();
                 bootsMeta.setColor(Color.BLACK);
@@ -59,7 +59,6 @@ public class TeamSelectMenuLis implements Listener {
                 player.getInventory().setBoots(boots);
                 player.getInventory().setLeggings(leggings);
                 player.getInventory().setChestplate(chestplate);
-
 
             }
 
