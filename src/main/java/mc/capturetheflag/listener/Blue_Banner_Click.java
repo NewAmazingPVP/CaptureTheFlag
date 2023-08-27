650517cd4e7ce0068825a78eddcc35c136d77b77
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-27 09:26:38 -0400

    needs fixing

--- src/main/java/mc/capturetheflag/listener/Blue_Banner_Click.java
+++ src/main/java/mc/capturetheflag/listener/Blue_Banner_Click.java
@@ -1,6 +1,8 @@
 package mc.capturetheflag.listener;
 
 import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -28,10 +30,21 @@ public class Blue_Banner_Click implements Listener {
             to.getBlock().setType(Material.AIR);
             ItemStack blueBanner = new ItemStack(Material.BLUE_BANNER);
             player.getEquipment().setItem(EquipmentSlot.HEAD, blueBanner);
+            player.getWorld().spawnParticle(Particle.REDSTONE, player.getLocation(), 10, 0.6, 0.6, 0.6, new Particle.DustOptions(Color.BLUE, 3));
             player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 999999999, 1, true));
+
+            Block block = event.getBlock();
+            World world = block.getWorld();
+
+            // Create a falling block entity with blue concrete block state
+            FallingBlock fallingBlock = world.spawnFallingBlock(block.getLocation(), Material.BLUE_CONCRETE.createBlockData());
+
+            // Set the velocity of the falling block
+            fallingBlock.setVelocity(new Vector(0, 1, 0));
+
             Sound sound = Sound.ENTITY_ENDER_DRAGON_GROWL;
             float volume = 1.0f;
-            float pitch = 1.0f;
+            float pitch = 0.0f;
             for (Player all : Bukkit.getOnlinePlayers()) {
                 all.playSound(all.getLocation(), sound, volume, pitch);
             }
