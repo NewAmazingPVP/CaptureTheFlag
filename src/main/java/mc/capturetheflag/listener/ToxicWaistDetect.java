633920ba3084c779f6609ad904e25e17d6417ec7
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-08-28 21:31:46 -0400

    fix xD

--- src/main/java/mc/capturetheflag/listener/ToxicWaistDetect.java
+++ src/main/java/mc/capturetheflag/listener/ToxicWaistDetect.java
@@ -1,6 +1,7 @@
 package mc.capturetheflag.listener;
 
 import jdk.tools.jlink.plugin.Plugin;
+import mc.capturetheflag.CaptureTheFlag;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,7 +11,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.Team;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -28,7 +31,8 @@ public class ToxicWaistDetect implements Listener {
 
         if (below.getBlock().getType() == Material.LIME_STAINED_GLASS) {
             player.playSound(player.getLocation(), "minecraft:block.lava.extinguish", 999999999999.0f, 1.0f);
-            player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 150, 55, true));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 200, 55, true));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 5, 55, true));
             player.spawnParticle(Particle.LAVA, player.getLocation().add(0.5, 1.0, 0.5), 10);
             String message = (ChatColor.YELLOW + "" + ChatColor.BOLD + player + ChatColor.GREEN + "" + ChatColor.BOLD + "Fell Into Toxic Waste!");
             Bukkit.broadcastMessage(message);
@@ -55,10 +59,13 @@ public class ToxicWaistDetect implements Listener {
             player.getInventory().setBoots(boots);
             player.getInventory().setLeggings(leggings);
             player.getInventory().setChestplate(chestplate);
-            new Thread(() -> {
-                try {
-                    Thread.sleep(10000);
-                    if(team.getName().equalsIgnoreCase("red")){
+
+
+            BukkitRunnable delayTask = new BukkitRunnable() {
+                @Override
+                public void run() {
+
+                    if (team.getName().equalsIgnoreCase("red")) {
                         ItemStack Rboots = new ItemStack(Material.LEATHER_BOOTS);
                         LeatherArmorMeta RbootsMeta = (LeatherArmorMeta) boots.getItemMeta();
                         RbootsMeta.setColor(Color.RED);
@@ -77,11 +84,11 @@ public class ToxicWaistDetect implements Listener {
                         RchestplateMeta.setColor(Color.RED);
                         RchestplateMeta.setDisplayName(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Red Armor");
                         Rchestplate.setItemMeta(RchestplateMeta);
+                        delayTask.runTaskLater(CaptureTheFlag, 20 * 20);
                     }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
                 }
-            }).start();
+            }
         }
     }
 }
+
